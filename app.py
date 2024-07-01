79e09f78f295e18535a72935c2deff19174035c8
Shrey Savaliya <shreynsavaliya@gmail.com>
2024-07-01 20:25:07 +0530

    Add Files

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+from extract_content import (extract_conversations, extract_sales_agent_timestamps, extract_customer_timestamps,
+                             extract_customer_dialogues, extract_sales_agent_dialogues)
+from sentiment_analysis import analyze_sentiment
+import plotly.express as px
+import streamlit as st
+app = Flask(__name__)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    
+    if file:
+        content = file.read().decode("utf-8")
+        # Process the file content here
+
+        # print("File content:", content)
+
+        sales_agent_conversations, customer_conversations = extract_conversations(content)
+        sales_agent_timestamps = extract_sales_agent_timestamps(content)
+        customer_timestamps = extract_customer_timestamps(content)
+        sales_agent_dialogues = extract_sales_agent_dialogues(sales_agent_conversations)
+        customer_dialogues = extract_customer_dialogues(customer_conversations)
+        sales_agent_sentiments = analyze_sentiment(sales_agent_dialogues)
+        customer_sentiments = analyze_sentiment(customer_dialogues)
+
+        # return jsonify({"Transcript": content})
+
+        # fig = create_piechart(sales_agent_sentiments)
+        # st.plotly_chart(fig)
+        # return fig
+
+        return jsonify({"message": "File received",
+                       "content": content,
+                        "sales_agent_conversations": sales_agent_conversations,
+                        "customer_conversations": customer_conversations,
+                        "sales_agent_timestamps": sales_agent_timestamps,
+                        "customer_timestamps": customer_timestamps,
+                        "sales_agent_dialogues": sales_agent_dialogues,
+                        "sales_agent_sentiments": sales_agent_sentiments,
+                        "customer_dialogues": customer_dialogues,
+                        "customer_sentiments": customer_sentiments
+                        }), 200
+
+#         visualize_sentiments(sales_agent_timestamps, sales_agent_sentiments,
+#                              customer_timestamps, customer_sentiments)
---
+++ sentiment_analysis.py
@@ -0,0 +1,65 @@
+from transformers import (pipeline, AutoTokenizer, AutoModelForSequenceClassification,
+                          LongformerTokenizer, LongformerForSequenceClassification, BigBirdTokenizer,
+                          BigBirdForSequenceClassification,
+                          DistilBertTokenizer, DistilBertForSequenceClassification)
+import torch
+from textblob import TextBlob
+
+# def analyze_sentiment(file_content):
+#     model_name = "google/bigbird-roberta-base"
+#     tokenizer = BigBirdTokenizer.from_pretrained(model_name)
+#     model = BigBirdForSequenceClassification.from_pretrained(model_name)
+#
+#     for line in file_content:
+#
+#         inputs = tokenizer(line, return_tensors='pt', max_length=4096, truncation=True)
+#
+#         with torch.no_grad():
+#             outputs = model(**inputs)
+#             logits = outputs.logits
+#             predicted_class = torch.argmax(logits).item()
+#
+#         labels = ['negative', 'neutral', 'positive']  # Assuming 3-class sentiment analysis
+#         print(f"Predicted class: {labels[predicted_class]}",
+#               f"Line: {line}")
+
+
+# def analyze_sentiment(file_content):
+#
+#     max_length = 1024
+#     sentiments = []
+#     for line in file_content:
+#         text = line[:max_length]
+#         blob = TextBlob(text)
+#         sentiment = blob.sentiment
+#         sentiments.append(sentiment.polarity)
+#     print(len(sentiments))
+#     return sentiments
+
+
+def analyze_sentiment(file_content):
+
+    try:
+        model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    except Exception as e:
+        print("Error loading model: {e}")
+        raise e
+
+    sentiments = []
+
+    for line in file_content:
+        text = tokenizer(line[:1024], return_tensors='pt')
+        output = model(**text)
+        scores = output.logits.softmax(dim=1).tolist()[0]
+        sentiment = {'Negative': scores[0], 'Neutral': scores[1], 'Positive': scores[2]}
+        sentiments.append(sentiment)
+
+    answers = []
+    for value in sentiments:
+        key = max(value, key=value.get)
+        value = value[key]
+        answers.append({key: value})
+
+    return answers
